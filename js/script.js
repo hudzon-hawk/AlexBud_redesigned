1694452dec33e50baf6576ef1e7e210f8773163b
hudzon-hawk <142737996+hudzon-hawk@users.noreply.github.com>
2023-08-20 18:03:39 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,77 @@
+var swiper = new Swiper('.about__slider', {
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true
+    },
+
+});
+
+
+var swiper = new Swiper('.projects__slider1', {
+    loop: true,
+    pagination: {
+        el: '.slider-projects__pagination',
+        clickable: true
+    },
+});
+
+
+var swiper = new Swiper('.projects__slider2', {
+    loop: true,
+    pagination: {
+        el: '.slider-projects__pagination',
+        clickable: true
+    },
+});
+
+
+
+var swiper = new Swiper('.projects__slider3', {
+    loop: true,
+    pagination: {
+        el: '.slider-projects__pagination',
+        clickable: true
+    },
+});
+
+
+
+var swiper4 = new Swiper('.projects__slider4', {
+    loop: true,
+    pagination: {
+        el: '..slider-projects__pagination',
+        clickable: true
+    },
+});
+
+var swiper4 = new Swiper('.projects__slider5', {
+    loop: true,
+    pagination: {
+        el: '.slider-projects__pagination',
+        clickable: true
+    },
+});
+
+var swiper4 = new Swiper('.projects__slider6', {
+    loop: true,
+    pagination: {
+        el: '.slider-projects__pagination',
+        clickable: true
+    },
+});
+
+var swiper4 = new Swiper('.projects__slider7', {
+    loop: true,
+    pagination: {
+        el: '.slider-projects__pagination',
+        clickable: true
+    },
+});
+var swiper4 = new Swiper('.projects__slider8', {
+    loop: true,
+    pagination: {
+        el: '.slider-projects__pagination',
+        clickable: true
+    },
+}); 
